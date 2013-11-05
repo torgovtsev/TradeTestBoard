4da2596ead2ff5aa505283fd6cb88e22bfcbe37f
torgovtsev <tsscsf@gmail.com>
2013-11-05 22:51:47 +0400

    TypedPanel class implementation

---
+++ src/main/java/com/google/code/jskills/utils/panel/TypedPanel.java
@@ -0,0 +1,37 @@
+package com.google.code.jskills.utils.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+public class TypedPanel<T> extends Panel {
+
+	private static final long serialVersionUID = -3251715062105985934L;
+
+	public TypedPanel(String id) {
+		super(id);
+	}
+
+	public TypedPanel(String id, IModel<?> model) {
+		super(id, model);
+	}
+
+	@SuppressWarnings("unchecked")
+	public IModel<T> getModel() {
+		IModel<?> defaultModel = this.getDefaultModel();
+		return defaultModel == null ? null : (IModel<T>) defaultModel;
+	}
+
+	public void setModel(IModel<T> model) {
+		this.setDefaultModel(model);
+	}
+
+	@SuppressWarnings("unchecked")
+	public T getModelObject() {
+		Object defaultModelObject = this.getDefaultModelObject();
+		return defaultModelObject == null ? null : (T) defaultModelObject;
+	}
+
+	public void setModelObject(T modelObject) {
+		this.setDefaultModelObject(modelObject);
+	}
+}
